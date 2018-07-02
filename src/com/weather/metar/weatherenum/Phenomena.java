658337edf7852a6ec538d75eecfcf5cd5251fba3
scarlett <jialishen7@gmail.com>
2018-07-02 11:14:57 +0800

    修正代码

--- src/com/weather/metar/weatherenum/Phenomena.java
+++ src/com/weather/metar/weatherenum/Phenomena.java
@@ -10,12 +10,12 @@ public enum Phenomena {
 	MI("MI", "浅"), BC("BC", "散片状"), DR("DR", "低吹"), BL("BL", "高吹"), SH("SH", "阵"), TS("TS", "雷"), FZ("FZ",
 			"冻"), PR("PR", "部分范围"),
 
-	DZ("DZ", "毛毛雨"), RA("RA", "雨"), SN("SN", "雪"), SG("SG", "米雪"), IC("IC", "冰针"), PE("PE", "冰粒"), GR("GR",
+	DZ("DZ", "毛毛雨"), RA("RA", "雨"), SN("SN", "雪"), SG("SG", "米雪"), IC("IC", "冰晶"), PL("PL", "冰粒"), GR("GR",
 			"冰雹"), GS("GS", "小冰雹"),
 
-	BR("BR", "轻雾"), FG("FG", "雾"), FU("FU", "烟"), VA("VA", "火山灰"), DU("DU", "大范围浮沉"), SA("SA", "沙"), HZ("HZ", "霾"),
+	BR("BR", "轻雾"), FG("FG", "雾"), FU("FU", "烟"), VA("VA", "火山灰"), DU("DU", "大范围浮尘"), SA("SA", "扬沙"), HZ("HZ", "霾"),
 
-	PO("BR", "尘/尘卷风"), SQ("FG", "飑"), FC("FU", "漏斗云"), SS("VA", "沙暴"), DS("DU", "尘暴");
+	PO("PO", "尘/尘卷风"), SQ("SQ", "飑"), FC("FC", "漏斗云"), SS("SS", "沙暴"), DS("DS", "尘暴");
 	private String code;
 	private String description;
 
